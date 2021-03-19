2e07d60f1af7e7820ee34928b7971c3ef5777c42
kittypickles9982 <admin@thatsfuckingstupid.org>
2021-03-18 20:00:03 -0400

    tjym,j

--- app/src/main/java/org/firstinspires/inspection/BuildConfig.java
+++ app/src/main/java/org/firstinspires/inspection/BuildConfig.java
@@ -1,15 +1,16 @@
 package org.firstinspires.inspection;
 
 public final class BuildConfig {
-  public static final String BUILD_TYPE = "release";
-  
+  // Set to release
+  public static final String BUILD_TYPE = "debug";
+  //set to false
   public static final boolean DEBUG = false;
   
   public static final String LIBRARY_PACKAGE_NAME = "org.firstinspires.inspection";
-  
-  public static final int VERSION_CODE = 76;
-  
-  public static final String VERSION_NAME = "14.0";
+  // Originally 76
+  public static final int VERSION_CODE = 77;
+  // Originally 14.0
+  public static final String VERSION_NAME = "14.0 Custom | Beta 0.1";
 }
 
 
